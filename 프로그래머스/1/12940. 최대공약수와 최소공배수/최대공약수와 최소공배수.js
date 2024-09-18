fb47859397f1ca77d73c787d939c83a6131b6d07
Yoon <108091177+c0d1ngkjy@users.noreply.github.com>
2024-09-19 02:32:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,16 @@
+function solution(n, m) {
+    return [gcd(n,m), lcm(n,m)];
+}
+
+function gcd(a, b) {
+    while (b !== 0) {
+        var temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
