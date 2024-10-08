851f1e5a32f6b1c2eea8bd4a09b89bdc4076f254
Yoon <108091177+c0d1ngkjy@users.noreply.github.com>
2024-10-08 15:44:51 +0900

    [Silver IV] Title: 큐, Time: 72 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+n = int(input())
+q = deque()
+
+for _ in range(n):
+    command = sys.stdin.readline().strip().split()
+    if command[0] == 'push':
+        q.append(command[1])
+    elif command[0] == 'pop':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(q))
+    elif command[0] == 'empty':
+        print(1 if not q else 0)
+    elif command[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif command[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
